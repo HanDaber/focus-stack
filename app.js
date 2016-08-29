9a519dfbd2467d1a60eb6a18eb2f4238d78a4d2e
HanDaber <handaber@gmail.com>
2016-08-28 19:12:49 -0700

    add test videos

--- app.js
+++ app.js
@@ -12,21 +12,21 @@ var express = require('express')
 
 var app = express();
 
-app.configure(function(){
+// app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser({uploadDir:'./public/video'}));
-  app.use(express.methodOverride());
-  app.use(app.router);
+//   app.use(express.favicon());
+//   app.use(express.logger('dev'));
+//   app.use(express.bodyParser({uploadDir:'./public/video'}));
+//   app.use(express.methodOverride());
+//   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
-});
+// });
 
-app.configure('development', function(){
-  app.use(express.errorHandler());
-});
+// app.configure('development', function(){
+//   app.use(express.errorHandler());
+// });
 
 app.get('/', routes.index);
 
